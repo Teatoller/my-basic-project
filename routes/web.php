8246eeefbe4677eab9f37f3395d0978a18f6f9be
Teatoller <steven.otieno1@gmail.com>
2020-06-19 18:03:18 +0300

    (views): Implement views
    - add route
    - add contact and about view
    - pass data into view
    - add layout file
    - extend layout
    - add home page
    
    [Finishes #001]

--- routes/web.php
+++ routes/web.php
@@ -11,8 +11,35 @@ use Illuminate\Support\Facades\Route;
 | routes are loaded by the RouteServiceProvider within a group which
 | contains the "web" middleware group. Now create something great!
 |
-*/
+ */
 
 Route::get('/', function () {
     return view('welcome');
+});
+
+// Route::get('/contact', function () {
+//     return view('contact');
+// });
+
+// Route::get('/about', function () {
+//     return view('about');
+// });
+
+Route::view('/', 'home');
+
+Route::view('/contact', 'contact');
+
+Route::view('/about', 'about');
+
+Route::get('/customers', function () {
+
+    $customers = [
+        'John Doe',
+        'Jane Doe',
+        'Bob the builder',
+    ];
+
+    return view('internals.customer', [
+        'customers'=>$customers,
+    ]);
 });
