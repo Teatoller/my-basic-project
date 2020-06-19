0a08348af0d143f754c222802894a6d3e84f9d1c
Teatoller <steven.otieno1@gmail.com>
2020-06-19 21:07:31 +0300

    (model):Implement model
    - add sqlite database
    - add customer model
    - run migration
    - add customer controller
    - add controller route
    - fetch data from sqlite database
    - add logic to persist customer data
    - add post route
    - add customer form
    - update readme file
    
    [Finishes #002]

---
+++ app/Http/Controllers/CustomersController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Customer;
+use Illuminate\Http\Request;
+
+class CustomersController extends Controller
+{
+    public function list()
+    {
+        $customers = Customer::all();
+
+        return view('internals.customer', [
+            'customers'=>$customers,
+        ]);
+    }
+
+    public function store(Request $request)
+    {
+        $customer = new Customer();
+        $customer->name = $request->name;
+        $customer->email = $request->email;
+        $customer->save();
+
+        return back();
+    }
+}
--- resources/views/internals/customer.blade.php
+++ resources/views/internals/customer.blade.php
@@ -3,10 +3,23 @@
 @section('content')
 <h1>Customers</h1>
 
+<form action="customers" method="POST" class="pb-5">
+
+    <div class="input-group">
+        <p>Name:</p>
+    <input type="text" name="name" >
+    <p>Email:</p>
+    <input type="text" name="email" >
+    </div>
+
+    <button type="submit">Add Customer</button>
+
+    @csrf
+</form>
 <ul>
  <!-- blade syntax -->
 @foreach($customers as $customer)
-    <li>{{$customer}}</li>
+    <li>{{$customer->name}}({{$customer->email}})</li>
 @endforeach
 
 </ul>
